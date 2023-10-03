78a3a8584654d627c185a70b3bebd85261a395d9
greg <pearsong35@gmail.com>
2023-10-03 19:15:23 -0400

    working on demo

--- webNotification/web/src/App.tsx
+++ webNotification/web/src/App.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Notify} from './component/notify/notify';
 
 function App() {
-  return (
+  const [userResponded, setUserResponded] = useState(false); 
+
+  function notifyUser() {
+    const notificationText = "thank you....";
+    if(!("Notification" in window)) {
+        alert("Browser not able");
+    } else if(Notification.permission === "granted") {
+        const notification = new Notification(notificationText)
+    } else if(Notification.permission !== "denied") {
+        Notification.requestPermission().then((permission) =>{
+            if(permission === "granted") {
+                const notification = new Notification(notificationText)
+            }
+        })
+    }
+  };
+
+  function enableNotifsAndClose() {
+    notifyUser();
+    // setUserResponded(true);
+  }
+
+  function disableNotifsAndClose() {
+    return "Hello";
+  }
+  return (!(userResponded) && !(Notification.permission === "granted")) ? (
     <div className="App-background">
       <header className="App-header">
          Web Notification
       </header>
       <div className='App-Text'>
         <p >
+          <br />
           Sample Notification Application (Need a Buttonpress) :
+          <br />
+            <button type="button" onClick={enableNotifsAndClose}>Enable Notification</button>
+          <br /> 
+          <br />
+            <button type="button" onClick={disableNotifsAndClose}>Disable Notification</button>
         </p>
         <Notify />
       </div>
     </div>
-  );
+  ) : (Notification.permission === "granted") ? (
+    <div>
+      <p>Got Here</p>
+    </div>
+  ) : <></>;
 }
 
 export default App;
---
+++ webNotification/web/src/component/notify/pushNotify.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export class PushNotify extends React.Component {
+    render() {
+        const publicKey = 'BOnNUoPIf3z2uvcY6aXrS0mmH_FfslcjdETmOYYWYgW-ldgPIJyQdMgpQ0Q5L6mcYNqbO68ImPEJhnhv1EyFgvM';
+
+        // const [userResponded, setUserResponded] = useState(false); 
+        
+        return (
+            <div>
+                <button type="button" >Click Me</button>
+            </div>
+        );
+    }
+}
